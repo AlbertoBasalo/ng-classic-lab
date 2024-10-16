e94952f76bdc4b658e57c20afd046e2e4823308e
Alberto Basalo <albertobasalo@hotmail.com>
2024-10-16 09:15:53 +0200

    feat: in memory IoC

--- AstroBookings/src/app/core/core.module.ts
+++ AstroBookings/src/app/core/core.module.ts
@@ -1,10 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { LaunchesMemRepository } from '@app/services/launches.mem.repository';
+import { LaunchesRepository } from '@app/services/launches.repository';
 import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   imports: [CommonModule, LayoutModule],
   exports: [LayoutModule],
-  providers: [],
+  providers: [
+    {
+      provide: LaunchesRepository,
+      useClass: LaunchesMemRepository,
+    },
+  ],
 })
 export class CoreModule {}
--- AstroBookings/src/app/shared/services/launches.repository.ts
+++ AstroBookings/src/app/shared/services/launches.repository.ts
@@ -1,47 +1,10 @@
 import { Injectable } from '@angular/core';
-import { delay, Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { LaunchDto, LaunchStatus } from '../models/launch.dto';
 
 // npm run ng -- g s shared/services/launches-repository
 
-@Injectable({
-  providedIn: 'root',
-})
-export class LaunchesRepository {
-  private readonly launches: LaunchDto[] = [
-    {
-      id: '1',
-      agencyId: '1',
-      rocketId: '1',
-      date: new Date(2029, 5, 1),
-      mission: 'Moon Landing',
-      destination: 'Moon',
-      pricePerSeat: 100,
-      status: 'scheduled',
-    },
-    {
-      id: '2',
-      agencyId: '2',
-      rocketId: '2',
-      date: new Date(2033, 8, 2),
-      mission: 'Mars Flight',
-      destination: 'Mars',
-      pricePerSeat: 200,
-      status: 'scheduled',
-    },
-    {
-      id: '3',
-      agencyId: '3',
-      rocketId: '3',
-      date: new Date(2037, 11, 3),
-      mission: 'Jupiter Mission',
-      destination: 'Jupiter',
-      pricePerSeat: 300,
-      status: 'aborted',
-    },
-  ];
-
-  getLaunchesByStatus$(status: LaunchStatus): Observable<LaunchDto[]> {
-    return of(this.launches.filter((launch) => launch.status === status)).pipe(delay(2000));
-  }
+@Injectable()
+export abstract class LaunchesRepository {
+  abstract getLaunchesByStatus$(status: LaunchStatus): Observable<LaunchDto[]>;
 }
