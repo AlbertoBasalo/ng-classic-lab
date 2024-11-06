f20eb746bb34c7c9594f201df1bf17380f8fe845
Alberto Basalo <albertobasalo@hotmail.com>
2024-11-06 10:43:16 +0100

    feat: bookings sub forms groups

--- AstroBookings/src/app/routes/launches/bookings-form.component.ts
+++ AstroBookings/src/app/routes/launches/bookings-form.component.ts
@@ -1,16 +1,14 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-import {
-  AbstractControl,
-  FormArray,
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { BookingDto } from '@app/models/booking.dto';
 import { LaunchDto } from '@app/models/launch.dto';
 import { PassengerDto } from '@app/models/passenger.dto';
 
+/**
+ * A component that renders a form for booking a launch
+ * - Needs a launch object as input
+ * - Emits a booking and passengers when the form is submitted
+ */
 @Component({
   selector: 'app-bookings-form',
   template: `
@@ -28,37 +26,37 @@ import { PassengerDto } from '@app/models/passenger.dto';
           <ng-container [formGroupName]="i">
             <app-control
               [form]="bookingForm"
+              arrayName="passengers"
+              [arrayIndex]="i"
               formControlName="contactPhone"
-              formArrayNameStr="passengers"
-              [formArrayIndexNbr]="i"
             />
             <app-control
               [form]="bookingForm"
-              formControlName="contactEmail"
               type="email"
-              formArrayNameStr="passengers"
-              [formArrayIndexNbr]="i"
+              arrayName="passengers"
+              [arrayIndex]="i"
+              formControlName="contactEmail"
             />
             <app-control
               [form]="bookingForm"
+              arrayName="passengers"
+              [arrayIndex]="i"
               formControlName="emergencyContact"
-              formArrayNameStr="passengers"
-              [formArrayIndexNbr]="i"
             />
             <div formGroupName="travelPreferences">
               <app-control
                 [form]="bookingForm"
+                arrayName="passengers"
+                [arrayIndex]="i"
+                groupName="travelPreferences"
                 formControlName="preferredDestination"
-                formArrayNameStr="passengers"
-                formGroupNameStr="travelPreferences"
-                [formArrayIndexNbr]="i"
               />
               <app-control
                 [form]="bookingForm"
+                arrayName="passengers"
+                [arrayIndex]="i"
+                groupName="travelPreferences"
                 formControlName="dietaryRestrictions"
-                formArrayNameStr="passengers"
-                formGroupNameStr="travelPreferences"
-                [formArrayIndexNbr]="i"
               />
             </div>
           </ng-container>
@@ -70,16 +68,31 @@ import { PassengerDto } from '@app/models/passenger.dto';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BookingsFormComponent {
+  // Inputs
+  /**
+   * The launch to book
+   */
   @Input() launch!: LaunchDto;
+  // Outputs
+  /**
+   * Emits a booking and passengers when the form is submitted
+   */
   @Output() book = new EventEmitter<{
     booking: Partial<BookingDto>;
     passengers: Partial<PassengerDto>[];
   }>();
 
+  /**
+   * The form for the booking
+   * - Has a number of seats control
+   * - Has a passengers array control
+   */
   bookingForm = this.formBuilder.group({
     numberOfSeats: new FormControl(0, [Validators.required, Validators.min(1), Validators.max(10)]),
     passengers: this.formBuilder.array([]),
   });
+
+  // Controls getters
 
   get numberOfSeats(): AbstractControl {
     return this.bookingForm.get('numberOfSeats')!;
@@ -89,26 +102,13 @@ export class BookingsFormComponent {
     return this.bookingForm.get('passengers') as FormArray;
   }
 
-  constructor(private readonly formBuilder: FormBuilder) {
-    this.addPassengerForm();
-  }
+  constructor(private readonly formBuilder: FormBuilder) {}
 
-  private createPassengerForm(): FormGroup {
-    return this.formBuilder.group({
-      contactPhone: ['', [Validators.required]],
-      contactEmail: ['', [Validators.required, Validators.email]],
-      emergencyContact: [''],
-      travelPreferences: this.formBuilder.group({
-        preferredDestination: [''],
-        dietaryRestrictions: [''],
-      }),
-    });
-  }
+  // Event handlers
 
-  private addPassengerForm(): void {
-    this.passengers.push(this.createPassengerForm());
-  }
-
+  /**
+   * Adds or removes passenger forms to the passengers array based on the number of seats
+   */
   onSeatsChange(): void {
     const seats = this.numberOfSeats.value || 0;
     const currentForms = this.passengers.length;
@@ -124,22 +124,42 @@ export class BookingsFormComponent {
     }
   }
 
+  /**
+   * Submits the form
+   */
   onSubmit(): void {
-    if (this.bookingForm.valid) {
-      const formValue = this.bookingForm.value;
-      const booking: Partial<BookingDto> = {
-        launchId: this.launch.id,
-        numberOfSeats: formValue.numberOfSeats!,
-        status: 'pending',
-        totalPrice: this.launch.pricePerSeat * formValue.numberOfSeats!,
-        passengers: [], // IDs will be assigned after passenger creation
-      };
-      const newBooking = {
-        booking,
-        passengers: formValue.passengers as Partial<PassengerDto>[],
-      };
-      console.log('🚀 newBooking', newBooking);
-      this.book.emit(newBooking);
-    }
+    if (this.bookingForm.invalid) return;
+    const numberOfSeats = this.numberOfSeats.value || 0;
+    if (numberOfSeats === 0) return;
+    const formValue = this.bookingForm.value;
+    const booking: Partial<BookingDto> = {
+      launchId: this.launch.id,
+      numberOfSeats,
+      status: 'pending',
+      totalPrice: this.launch.pricePerSeat * numberOfSeats,
+      passengers: [], // IDs will be assigned after passenger creation
+    };
+    const newBooking = {
+      booking,
+      passengers: formValue.passengers as Partial<PassengerDto>[],
+    };
+    console.log('🚀 newBooking', newBooking);
+    this.book.emit(newBooking);
+  }
+
+  /**
+   * Adds a new passenger form to the passengers array
+   */
+  private addPassengerForm(): void {
+    const newPassengerForm = this.formBuilder.group({
+      contactPhone: ['', [Validators.required]],
+      contactEmail: ['', [Validators.required, Validators.email]],
+      emergencyContact: [''],
+      travelPreferences: this.formBuilder.group({
+        preferredDestination: [''],
+        dietaryRestrictions: [''],
+      }),
+    });
+    this.passengers.push(newPassengerForm);
   }
 }
--- AstroBookings/src/app/shared/ui/control.block.ts
+++ AstroBookings/src/app/shared/ui/control.block.ts
@@ -6,6 +6,12 @@ import {
   NG_VALUE_ACCESSOR,
 } from '@angular/forms';
 
+/**
+ * A component that renders a form control with a label and an input element and an error message if the control is invalid
+ * - Needs a form, and formControlName as inputs
+ * - Accepts label, placeholder, and type as inputs
+ * - It also accepts arrayName, arrayIndex, and groupName inputs for nested arrays and groups
+ */
 @Component({
   selector: 'app-control',
   providers: [
@@ -21,9 +27,9 @@ import {
         {{ label || (formControlName | titlecase) }} <span *ngIf="hasError()">🚨</span>
       </label>
       <input
-        [name]="formControlName"
         [type]="type"
         [id]="formControlName"
+        [name]="formControlName"
         [placeholder]="placeholder || label || formControlName"
         autocomplete="off"
         [value]="value"
@@ -35,6 +41,7 @@ import {
   `,
 })
 export class ControlBlock implements ControlValueAccessor {
+  // Inputs
   /**
    * The form to which the control belongs
    * - This is mandatory!
@@ -47,10 +54,12 @@ export class ControlBlock implements ControlValueAccessor {
   @Input() formControlName!: string;
   /**
    * The text for the label
+   * - Defaults to the formControlName
    */
   @Input() label = '';
   /**
    * The placeholder used as a hint
+   * - Defaults to the label or formControlName
    */
   @Input() placeholder = '';
   /**
@@ -59,58 +68,69 @@ export class ControlBlock implements ControlValueAccessor {
    */
   @Input() type = 'text';
 
+  // Group and array inputs
   /**
    * The name of the array to search for the control
    * - Optional, only for sub-arrays
    */
-  @Input() formArrayNameStr?: string;
+  @Input() arrayName?: string;
   /**
    * The index of the control in the array
    * - Optional, only for sub-arrays
    */
-  @Input() formArrayIndexNbr?: number;
+  @Input() arrayIndex?: number;
   /**
    * The name of the group to search for the control
    * - Optional, only for nested groups
    */
-  @Input() formGroupNameStr?: string;
+  @Input() groupName?: string;
 
   /**
    * The current value of the control
    */
   value: unknown;
+
   /**
    * The function to emit the change event
    */
-  emitChange: any;
+  private changeCallBack: any;
   /**
    * The function to emit the touch event
    */
-  emitTouch: any;
+  private touchCallBack: any;
 
   constructor() {}
 
-  writeValue(obj: any): void {
-    this.value = obj;
+  // ControlValueAccessor methods
+
+  /**
+   * Stores the value of the control
+   * - This method is called by the ReactiveFormsModule when the value changes programmatically
+   * @param newValue - The value to store
+   */
+  writeValue(newValue: any): void {
+    this.value = newValue;
   }
 
   /**
    * Stores a function to be called when the control value changes
    * - This method is called once by the ReactiveFormsModule when the component is initialized
-   * @param fn - The function to emit the change event
+   * @param changeCallBack - The function to emit the change event
    */
-  registerOnChange(fn: any): void {
-    this.emitChange = fn;
+  registerOnChange(changeCallBack: any): void {
+    this.changeCallBack = changeCallBack;
   }
 
   /**
    * Stores a function to be called when the control is touched
    * - This method is called once by the ReactiveFormsModule when the component is initialized
-   * @param fn - The function to emit the touch event
+   * @param touchCallBack - The function to emit the touch event
    */
-  registerOnTouched(fn: any): void {
-    this.emitTouch = fn;
+  registerOnTouched(touchCallBack: any): void {
+    this.touchCallBack = touchCallBack;
   }
+
+  // Event handlers
 
   /**
    * The event handler for the input event
@@ -118,16 +138,25 @@ export class ControlBlock implements ControlValueAccessor {
    * @param event - The InputEvent to handle
    */
   onInput(event: any) {
-    this.value = event.target.value;
-    this.emitChange(this.value);
-    this.emitTouch();
+    const target: HTMLInputElement = event.target as HTMLInputElement;
+    this.value = target.value;
+    this.changeCallBack(this.value);
+    this.touchCallBack();
   }
 
   // Utility methods
 
+  /**
+   * Checks if the control has an error
+   * @returns true if the control has an error, false otherwise
+   */
   hasError() {
     return this.getControl().invalid;
   }
+  /**
+   * Checks if the control should be treated as invalid
+   * @returns undefined if the control is pristine, true if the control is invalid, false otherwise
+   */
   getInvalid(): boolean | undefined {
     if (this.getControl().pristine) {
       // user has not interacted with the control yet
@@ -135,22 +164,27 @@ export class ControlBlock implements ControlValueAccessor {
     }
     return this.getControl().invalid;
   }
+  /**
+   * Gets the error message of the control
+   * @returns the error message of the control
+   */
   getErrorMessage() {
     return JSON.stringify(this.getControl().errors || {});
   }
+  /**
+   * Gets the control based on the path at the form
+   * - uses the arrayName, arrayIndex, groupName and formControlName to build the path
+   * @returns the control
+   */
   private getControl() {
-    //console.log('getting control', this.formControlName);
-    let control: AbstractControl | null = this.form.get(this.formControlName);
+    const controlPath = [];
+    if (this.arrayName !== undefined) controlPath.push(this.arrayName);
+    if (this.arrayIndex !== undefined) controlPath.push(this.arrayIndex);
+    if (this.groupName !== undefined) controlPath.push(this.groupName);
+    controlPath.push(this.formControlName);
+    let control: AbstractControl | null = this.form.get(controlPath);
     if (!control) {
-      const searchArray = [];
-      if (this.formArrayNameStr) searchArray.push(this.formArrayNameStr);
-      if (this.formArrayIndexNbr !== undefined) searchArray.push(this.formArrayIndexNbr);
-      if (this.formGroupNameStr) searchArray.push(this.formGroupNameStr);
-      searchArray.push(this.formControlName);
-      control = this.form.get(searchArray);
-      if (!control) {
-        throw new Error(`Control ${this.formControlName} not found`);
-      }
+      throw new Error(`Control ${controlPath} not found`);
     }
     return control;
   }
